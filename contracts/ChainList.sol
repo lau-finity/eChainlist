9109a698e9ca042db55941da23cbab758ae4fd72
Kelvin Lau <kelvinlau@Kelvins-MacBook-Pro.local>
2017-11-12 12:14:15 -0700

    added extra test cases for buying and selling multiple articles

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -1,58 +1,102 @@
 pragma solidity ^0.4.11;
 
 contract ChainList {
-  // State variables
-  address seller;
-  address buyer;
-  string name;
-  string description;
-  uint256 price;
+  // Create new article struct to store custom types
+  struct Article {
+    uint id;
+    address seller;
+    address buyer;
+    string name;
+    string description;
+    uint256 price;
+  }
+
+  // Initialize variables
+  mapping(uint => Article) public articles;
+  uint articleCounter;
 
   // Declare events
-  event sellArticleEvent(address indexed _seller, string _name, uint256 _price);
-  event buyArticleEvent(address indexed _seller, address indexed _buyer, string _name, uint256 _price);
+  event sellArticleEvent(uint indexed _id, address indexed _seller, string _name, uint256 _price);
+  event buyArticleEvent(uint indexed _id, address indexed _seller, address indexed _buyer, string _name, uint256 _price);
 
   // Sell an article
   function sellArticle(string _name, string _description, uint256 _price)
   public {
-    seller = msg.sender;
-    name = _name;
-    description = _description;
-    price = _price;
-    sellArticleEvent(seller, name, price);
+    articleCounter++;
+
+    articles[articleCounter] = Article(
+      articleCounter,
+      msg.sender,
+      0x0,
+      _name,
+      _description,
+      _price
+      );
+
+    sellArticleEvent(articleCounter, msg.sender, _name, _price);
   }
 
-  // Get the article
-  function getArticle() public constant returns (
-    address _seller,
-    address _buyer,
-    string _name,
-    string _description,
-    uint256 _price) {
-      return(seller, buyer, name, description, price);
+  // Fetches the number of articles in the contract
+  function getNumberOfArticles() public constant returns (uint) {
+    return articleCounter;
+  }
+
+  // Fetches and returns all article ids that are on sale
+  function getArticlesForSale() public constant returns (uint[]) {
+    // Check if there is at least one article
+    require(articleCounter > 0);
+
+    // Prepare output arrays
+    // NOTE: uint[] is stored in memory which is less expensive than storing it into storage
+    uint[] memory articleIds = new uint[](articleCounter);
+
+    uint numberOfArticlesForSale = 0;
+
+    // Loop through articles
+    for (uint i = 1; i <= articleCounter; i++) {
+      // Keep only the ID for the article not already sold
+      if (articles[i].buyer == 0x0) {
+        articleIds[numberOfArticlesForSale] = articles[i].id;
+        numberOfArticlesForSale++;
+      }
+    }
+
+    // Copy articleIds array into smaller forSale array
+    uint[] memory forSale = new uint[](numberOfArticlesForSale);
+    for (uint j = 0; j < numberOfArticlesForSale; j++) {
+      forSale[j] = articleIds[j];
+    }
+
+    return (forSale);
   }
 
   // Buy an article
-  function buyArticle() payable public {
-    // Check if article is up for sellArticle
-    require(seller != 0x0);
+  function buyArticle(uint _id) payable public {
+    // Check if there's at least one article
+    require(articleCounter > 0);
 
-    // Check if article wasn't already solid
-    require(buyer == 0x0);
+    // Check if article exists
+    require(_id > 0 && _id <= articleCounter);
 
-    // Do not allow seller to buy its own article
-    require(msg.sender != seller);
+    // retreive the article
+    Article storage article = articles[_id];
 
-    // Check if value sent corresponds to article price
-    require(msg.value == price);
+    // Check if article has not already been sold
+    require(article.buyer == 0x0);
+
+    // Don't allow seller to buy own article
+    require(article.seller != msg.sender);
+
+    // Check if value sent corresponds to the article price
+    require(article.price == msg.value);
 
     // Keep buyer's information
-    buyer = msg.sender;
+    article.buyer = msg.sender;
 
-    // Buyer can now buy article
-    seller.transfer(msg.value);
+    // Buyer can buy the article
+    article.seller.transfer(msg.value);
 
-    // Trigger buyArticleEvent
-    buyArticleEvent(seller, buyer, name, price);
+    // Trigger event
+    buyArticleEvent(_id, article.seller, article.buyer, article.name, article.price);
   }
 }
--- test/ChainListExceptions.js
+++ test/ChainListExceptions.js
@@ -1,136 +1,189 @@
 // Contract to be tested
-
 var ChainList = artifacts.require("./ChainList.sol");
 
-// Test suite
+// Test suite + cases
 contract('ChainList', function(accounts) {
   var chainListInstance;
   var seller = accounts[1];
   var buyer = accounts[2];
+  var articleId = 1;
   var articleName = "article 1";
   var articleDescription = "Description for article 1";
   var articlePrice = 10;
 
-  // Test case 1: no article is for sale
+  // Test case 1: getting articles for sale when no article for sale yet
   it(
-    "should thrown exception if you try to buy an article and it's not up for sale",
+    "should throw an exception if you try to get articles for sale when there is no article at all",
     function() {
       return ChainList.deployed().then(function(instance) {
-        chainListInstance = instance;
-        return chainListInstance.buyArticle({
-          from: buyer,
-          value: web3.toWei(articlePrice, "ether")
-        });
-      }).then(assert.fail).catch(function(error) {
-        assert(error.message.indexOf('invalid opcode') >= 0,
-          "error should be invalid opcode");
-      }).then(function() {
-        return chainListInstance.getArticle.call();
-      }).then(function(data) {
-        // make sure the contract state was not altered
-        assert.equal(data[0], 0x0, "seller must be empty");
-        assert.equal(data[1], 0x0, "buyer must be empty");
-        assert.equal(data[2], '', "article name must be empty");
-        assert.equal(data[3], '', "article description must be empty");
-        assert.equal(data[4].toNumber(), 0, "article price must be 0");
-      });
+          chainListInstance = instance;
+          return chainListInstance.getArticlesForSale();
+        }).then(assert.fail)
+        .catch(function(error) {
+          assert(error.message.indexOf('invalid opcode') >= 0,
+            "error message must contain invalid opcode");
+        })
     });
 
-  // Test case 2: cannot buy the same article you're selling
-  it("should throw exception if you try to buy your own article", function() {
-    return ChainList.deployed().then(function(instance) {
-      chainListInstance = instance;
-      return chainListInstance.sellArticle(articleName,
-        articleDescription,
-        web3.toWei(articlePrice, "ether"), {
-          from: seller
-        });
-    }).then(function(receipt) {
-      return chainListInstance.buyArticle({
-        from: seller,
-        bslur: web3.toWei(articlePrice, "ether")
-      });
-    }).then(assert.fail).catch(function(error) {
-      assert(error.message.indexOf('invalid opcode') >= 0,
-        "error should be invalid opcode");
-    }).then(function() {
-      return chainListInstance.getArticle.call();
-    }).then(function(data) {
-      // make sure the contract state still holds
-      assert.equal(data[0], seller, "seller must be " + seller);
-      assert.equal(data[1], 0x0, "buyer must be empty");
-      assert.equal(data[2], articleName, "article name must be " +
-        articleName);
-      assert.equal(data[3], articleDescription,
-        "article description must be " + articleDescription);
-      assert.equal(data[4].toNumber(), web3.toWei(articlePrice,
-        "ether"), "article price must be " + web3.toWei(
-        articlePrice, "ether"));
-    });
-  });
-
-  // Test case 3: buying article for different value from its price
-  // TODO: fix this, allow buyer to buy article from seller if they have more than seller's price
-  // Current: only accepts transaction if buy ether == sell ether
+  // Test case 2: buying an article when no article for sale yet
   it(
-    "should throw exception if you try to buy an article for value different from its price",
+    "should throw an exception if you try to buy an article when there is no article for sale",
     function() {
       return ChainList.deployed().then(function(instance) {
-        chainListInstance = instance;
-        return chainListInstance.buyArticle({
-          from: buyer,
-          value: web3.toWei(articlePrice + 1, "ether")
+          chainListInstance = instance;
+          return chainListInstance.buyArticle(articleId, {
+            from: buyer,
+            value: web3.toWei(articlePrice, "ether")
+          });
+        }).then(assert.fail)
+        .catch(function(error) {
+          assert(error.message.indexOf('invalid opcode') >= 0,
+            "error message must contain invalid opcode");
+        }).then(function() {
+          return chainListInstance.getNumberOfArticles();
+        }).then(function(data) {
+          //make sure sure the contract state was not altered
+          assert.equal(data.toNumber(), 0,
+            "number of articles must be zero");
         });
-      }).then(assert.fail).catch(function(error) {
-        assert(error.message.indexOf('invalid opcode') >= 0,
-          "error should be invalid opcode");
-      }).then(function() {
-        return chainListInstance.getArticle.call();
-      }).then(function(data) {
-        // make sure the contract state still holds
-        assert.equal(data[0], seller, "seller must be " + seller);
-        assert.equal(data[1], 0x0, "buyer must be empty");
-        assert.equal(data[2], articleName, "article name must be " +
-          articleName);
-        assert.equal(data[3], articleDescription,
-          "article description must be " + articleDescription);
-        assert.equal(data[4].toNumber(), web3.toWei(articlePrice,
-          "ether"), "article price must be " + web3.toWei(
-          articlePrice, "ether"));
-      });
     });
 
-  // Test case 4: cannot buy article that has been already sold
+  // Test case 3: buying an article that does not exist
   it(
-    "should throw exception if you try to buy article that has already been sold",
+    "should throw an exception if you try to buy an article that does not exist",
     function() {
       return ChainList.deployed().then(function(instance) {
-        chainListInstance = instance;
-        return chainListInstance.buyArticle({
-          from: buyer,
-          value: web3.toWei(articlePrice, "ether")
+          chainListInstance = instance;
+          return chainListInstance.sellArticle(articleName,
+            articleDescription, web3.toWei(articlePrice, "ether"), {
+              from: seller
+            });
+        }).then(function(receipt) {
+          return chainListInstance.buyArticle(2, {
+            from: buyer,
+            value: web3.toWei(articlePrice, "ether")
+          });
+        }).then(assert.fail)
+        .catch(function(error) {
+          assert(error.message.indexOf('invalid opcode') >= 0,
+            "error message must contain invalid opcode");
+        }).then(function() {
+          return chainListInstance.articles(articleId);
+        }).then(function(data) {
+          assert.equal(data[0].toNumber(), articleId,
+            "article id must be " + articleId);
+          assert.equal(data[1], seller, "seller must be " + seller);
+          assert.equal(data[2], 0x0, "buyer must be empty");
+          assert.equal(data[3], articleName, "article name must be " +
+            articleName);
+          assert.equal(data[4], articleDescription,
+            "article description must be " + articleDescription);
+          assert.equal(data[5].toNumber(), web3.toWei(articlePrice,
+            "ether"), "article price must be " + web3.toWei(
+            articlePrice, "ether"));
         });
-      }).then(function() {
-        return chainListInstance.buyArticle({
-          from: web3.eth.accounts[0],
-          value: web3.toWei(articlePrice, "ether")
+    });
+
+  // Test case 4: buying an article you are selling
+  it("should throw an exception if you try to buy your own article",
+    function() {
+      return ChainList.deployed().then(function(instance) {
+          chainListInstance = instance;
+          return chainListInstance.buyArticle(articleId, {
+            from: seller,
+            value: web3.toWei(articlePrice, "ether")
+          });
+        }).then(assert.fail)
+        .catch(function(error) {
+          assert(error.message.indexOf('invalid opcode') >= 0,
+            "error message must contain invalid opcode");
+        }).then(function() {
+          return chainListInstance.articles(articleId);
+        }).then(function(data) {
+          //make sure sure the contract state was not altered
+          assert.equal(data[0].toNumber(), articleId,
+            "article id must be " + articleId);
+          assert.equal(data[1], seller, "seller must be " + seller);
+          assert.equal(data[2], 0x0, "buyer must be empty");
+          assert.equal(data[3], articleName, "article name must be " +
+            articleName);
+          assert.equal(data[4], articleDescription,
+            "article description must be " + articleDescription);
+          assert.equal(data[5].toNumber(), web3.toWei(articlePrice,
+            "ether"), "article price must be " + web3.toWei(
+            articlePrice, "ether"));
         });
-      }).then(assert.fail).catch(function(error) {
-        assert(error.message.indexOf('invalid opcode') >= 0,
-          "error should be invalid opcode");
-      }).then(function() {
-        return chainListInstance.getArticle.call();
-      }).then(function(data) {
-        // make sure the contract state still holds
-        assert.equal(data[0], seller, "seller must be " + seller);
-        assert.equal(data[1], buyer, "buyer must be empty");
-        assert.equal(data[2], articleName, "article name must be " +
-          articleName);
-        assert.equal(data[3], articleDescription,
-          "article description must be " + articleDescription);
-        assert.equal(data[4].toNumber(), web3.toWei(articlePrice,
-          "ether"), "article price must be " + web3.toWei(
-          articlePrice, "ether"));
-      });
+    });
+
+  // Test case 5: incorrect value
+  // TODO: currently only supports buying article for the same selling article price
+  // It should allow the customer to buy an article if they have more than the selling price,
+  // just give back the leftover change to the customer.
+  it(
+    "should throw an exception if you try to buy an article for a value different from its price",
+    function() {
+      return ChainList.deployed().then(function(instance) {
+          chainListInstance = instance;
+          return chainListInstance.buyArticle(articleId, {
+            from: buyer,
+            value: web3.toWei(articlePrice + 1, "ether")
+          });
+        }).then(assert.fail)
+        .catch(function(error) {
+          assert(error.message.indexOf('invalid opcode') >= 0,
+            "error message must contain invalid opcode");
+        }).then(function() {
+          return chainListInstance.articles(articleId);
+        }).then(function(data) {
+          //make sure sure the contract state was not altered
+          assert.equal(data[0].toNumber(), articleId,
+            "article id must be " + articleId);
+          assert.equal(data[1], seller, "seller must be " + seller);
+          assert.equal(data[2], 0x0, "buyer must be empty");
+          assert.equal(data[3], articleName, "article name must be " +
+            articleName);
+          assert.equal(data[4], articleDescription,
+            "article description must be " + articleDescription);
+          assert.equal(data[5].toNumber(), web3.toWei(articlePrice,
+            "ether"), "article price must be " + web3.toWei(
+            articlePrice, "ether"));
+        });
+    });
+
+  // Test case 6: article has already been sold
+  it(
+    "should throw an exception if you try to buy an article that has already been sold",
+    function() {
+      return ChainList.deployed().then(function(instance) {
+          chainListInstance = instance;
+          return chainListInstance.buyArticle(articleId, {
+            from: buyer,
+            value: web3.toWei(articlePrice, "ether")
+          });
+        }).then(function() {
+          return chainListInstance.buyArticle(articleId, {
+            from: web3.eth.accounts[0],
+            value: web3.toWei(articlePrice, "ether")
+          });
+        }).then(assert.fail)
+        .catch(function(error) {
+          assert(error.message.indexOf('invalid opcode') >= 0,
+            "error message must contain invalid opcode");
+        }).then(function() {
+          return chainListInstance.articles(articleId);
+        }).then(function(data) {
+          //make sure sure the contract state was not altered
+          assert.equal(data[0].toNumber(), articleId,
+            "article id must be " + articleId);
+          assert.equal(data[1], seller, "seller must be " + seller);
+          assert.equal(data[2], buyer, "buyer must be " + buyer);
+          assert.equal(data[3], articleName, "article name must be " +
+            articleName);
+          assert.equal(data[4], articleDescription,
+            "article description must be " + articleDescription);
+          assert.equal(data[5].toNumber(), web3.toWei(articlePrice,
+            "ether"), "article price must be " + web3.toWei(
+            articlePrice, "ether"));
+        });
     });
 });
